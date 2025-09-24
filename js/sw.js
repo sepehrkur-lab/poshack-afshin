debe623266e66250bff103df2cd09c3da2728c6d
sepehrkur-lab <sepehrkur@gmail.com>
2025-09-25 00:02:21 +0330

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,39 +1,21 @@
-const CACHE_NAME = "sepahr-shop-v2";
+const CACHE_NAME = 'sepahr-shop-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/products.html",
-  "/product-detail.html",
-  "/checkout.html",
-  "/about.html",
-  "/contact.html",
-  "/admin-login.html",
-  "/css/style.css",
-  "/js/script.js",
-  "/js/product-loader.js",
-  "/js/products.js",
-  "/manifest.json"
+  '/',
+  '/index.html',
+  '/products.html',
+  '/product-detail.html',
+  '/checkout.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/js/product-loader.js',
+  '/js/products.js'
 ];
-
-// نصب Service Worker
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
-// فعال‌سازی
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
-
-// واکشی
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
