9186bac3c33f274bd7c350fef4991be0f21b6085
sepehrkur-lab <sepehrkur@gmail.com>
2025-09-25 09:07:59 +0330

    Update Products.js

--- js/Products.js
+++ js/Products.js
@@ -7,7 +7,15 @@ const products = [
     price: 400000,
     category: "دخترانه",
     image: "InShot_20250923_152332625.png",
-    description: " شلوار ۱۰۰٪ لی دخترانه    ."
+    description: " شلوار ۱۰۰٪ لی دخترانه    .",
   },
   {
+    id: 2,
+    name: " ست زمستانی اسپرت  ",
+    oldPrice: 500000,
+    price: 350000,
+    category: "پسرانه",
+    image: "InShot_20250923_162717584.jpg",
+    description: " ست اسپرت طرح گوسفند داخل نخ پنبه برای زمستان و پائیز.  ",
+  },
 ];
