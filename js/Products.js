503966c3fffefb10cc4d67239ebe5ff0182dc62c
sepehrkur-lab <sepehrkur@gmail.com>
2025-09-25 10:44:48 +0330

    Update Products.js

--- js/Products.js
+++ js/Products.js
@@ -18,4 +18,14 @@ const products = [
     image: "https://cdn.imgurl.ir/uploads/v5624_InShot_20250923_162717584.jpg",
     description: " ست اسپرت طرح گوسفند داخل نخ پنبه برای زمستان و پائیز.  ",
   },
+    {
+    id: 3,
+    name: "شلوار لی اسپرت مدل جین  ",
+    oldPrice: 2300000,
+    price: 340000,
+    category: "نوزاد",
+    image: "https://cdn.imgurl.ir/uploads/a304010_InShot_20250923_160132962.png",
+    description: " شلوار لی نوزادی از یک سال تا چهار سال مناسب برای پائیز، در دو رنگ مشکی و سفید.  ",
+  },
 ];
+
